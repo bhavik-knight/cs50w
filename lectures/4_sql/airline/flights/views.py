84824673e72c0be3a5a3dae39f73b85247a8522b
Bhavik <15388512+bhavik-knight@users.noreply.github.com15388512+bhavik-knight@users.noreply.github.com>
2022-02-25 02:07:26 -0400

    created superuser - admind - to tweak with database, and more views for invidual flight

--- lectures/4_sql/airline/flights/views.py
+++ lectures/4_sql/airline/flights/views.py
@@ -2,8 +2,20 @@ from django.shortcuts import render
 
 from .models import Flight
 
+
 # Create your views here.
 def index(request):
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
+    })
+
+
+def flight(request, flight_id):
+    try:
+        flight_instance = Flight.objects.get(id=flight_id)
+    except Flight.DoesNotExist:
+        flight_instance = None
+
+    return render(request, "flights/flight.html", {
+        "flight": flight_instance
     })
