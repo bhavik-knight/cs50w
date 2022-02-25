aef5a497dd20fb7549f834cc82470a2f9b6a0609
Bhavik <15388512+bhavik-knight@users.noreply.github.com15388512+bhavik-knight@users.noreply.github.com>
2022-02-25 00:43:45 -0400

    create view - basic html page shows all the flights

--- lectures/4_sql/airline/flights/views.py
+++ lectures/4_sql/airline/flights/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
+from .models import Flight
 
 # Create your views here.
 def index(request):
-    pass
+    return render(request, "flights/index.html", {
+        "flights": Flight.objects.all()
+    })
